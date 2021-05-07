f0db0365603c5e4260a80aae50fac6d6ca05986d
202-08\admin <ojh789ojh@naver.com>
2021-05-07 14:19:58 +0900

    AnJoin() 완성

---
+++ SpringWorkspace/MyProject_App/src/main/java/com/hanul/app/controller/AnController.java
@@ -0,0 +1,54 @@
+package com.hanul.app.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hanul.app.command.AnCommand;
+import com.hanul.app.command.AnJoinCommand;
+
+@Controller
+public class AnController {
+	
+	AnCommand command;
+	
+	@RequestMapping(value="/anJoin", method = {RequestMethod.GET, RequestMethod.POST}  )
+	public String anJoin(HttpServletRequest req, Model model){
+		System.out.println("anJoin()");
+		
+		try {
+			req.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} 
+		
+		String id = (String) req.getParameter("id");
+		String passwd = (String) req.getParameter("passwd");
+		String name = (String) req.getParameter("name");
+		String phonenumber = (String) req.getParameter("phonenumber");
+		String address = (String) req.getParameter("address");
+		
+		System.out.println(id);
+		System.out.println(passwd);
+		System.out.println(name);
+		System.out.println(phonenumber);
+		System.out.println(address);
+		
+		model.addAttribute("id", id);
+		model.addAttribute("passwd", passwd);
+		model.addAttribute("name", name);
+		model.addAttribute("phonenumber", phonenumber);
+		model.addAttribute("address", address);
+		
+		command = new AnJoinCommand();
+		command.execute(model);
+		
+		
+		return "anJoin";
+	}
+}
---
+++ SpringWorkspace/MyProject_App/src/main/java/com/hanul/app/dao/AnDao.java
@@ -0,0 +1,68 @@
+package com.hanul.app.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class AnDao {
+
+	DataSource dataSource;
+	
+	public AnDao() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:/comp/env/team01");
+			/*dataSource = (DataSource) context.lookup("java:/comp/env/CSS");*/
+		} catch (NamingException e) {
+			e.getMessage();
+		}
+
+	}
+	
+	private void dbClose(Connection connection, PreparedStatement prepareStatement) {
+		try {				
+			if (prepareStatement != null) {
+				prepareStatement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int anJoin(String id, String passwd, String name, String phonenumber, String address) {
+		Connection connection = null;
+		PreparedStatement prepareStatement = null;
+		int state = -100;
+		
+		try {
+			connection = dataSource.getConnection();
+			String query = "insert into member(id, passwd, name, phonenumber, address) " + 
+			               "values('" + id + "', '" + passwd + "', '" + name + "', '" + 
+					        			phonenumber + "', '" + address + "' )";
+			System.out.println(query);
+			prepareStatement = connection.prepareStatement(query);
+			state = prepareStatement.executeUpdate();
+			
+			if (state > 0) {
+				System.out.println(state + " »ğÀÔ¼º°ø");				
+			} else {
+				System.out.println(state + " »ğÀÔ½ÇÆĞ");
+			}
+			
+		} catch (Exception e) {			
+			System.out.println(e.getMessage());
+		} finally {
+			dbClose(connection, prepareStatement);
+		}
+
+		return state;
+	}
+
+}
